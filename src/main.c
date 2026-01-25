deafae6a355ffd15a2f68c790618a3a5fe61c834
pkhead <chromosoze@gmail.com>
2026-01-25 00:29:29 -0600

    initial commit

---
+++ src/main.c
@@ -0,0 +1,105 @@
+#include <tonc.h>
+#include <gfx_player.h>
+#include <gfx_tileset.h>
+
+OBJ_ATTR obj_buffer[128];
+
+int main()
+{
+    irq_init(NULL);
+    irq_add(II_VBLANK, NULL);
+
+    // set up graphics state
+    oam_init(obj_buffer, 128);
+
+    pal_bg_mem[ 0] = 0x0000;
+    pal_bg_mem[ 1] = 0x28a3;
+    pal_bg_mem[ 2] = 0x288f;
+    pal_bg_mem[ 3] = 0x2a00;
+    pal_bg_mem[ 4] = 0x1955;
+    pal_bg_mem[ 5] = 0x254b;
+    pal_bg_mem[ 6] = 0x6318;
+    pal_bg_mem[ 7] = 0x77df;
+    pal_bg_mem[ 8] = 0x241f;
+    pal_bg_mem[ 9] = 0x029f;
+    pal_bg_mem[10] = 0x13bf;
+    pal_bg_mem[11] = 0x1b80;
+    pal_bg_mem[12] = 0x7ea5;
+    pal_bg_mem[13] = 0x4dd0;
+    pal_bg_mem[14] = 0x55df;
+    pal_bg_mem[15] = 0x573f;
+
+    pal_obj_mem[ 0] = 0x0000;
+    pal_obj_mem[ 1] = 0x28a3;
+    pal_obj_mem[ 2] = 0x288f;
+    pal_obj_mem[ 3] = 0x2a00;
+    pal_obj_mem[ 4] = 0x1955;
+    pal_obj_mem[ 5] = 0x254b;
+    pal_obj_mem[ 6] = 0x6318;
+    pal_obj_mem[ 7] = 0x77df;
+    pal_obj_mem[ 8] = 0x241f;
+    pal_obj_mem[ 9] = 0x029f;
+    pal_obj_mem[10] = 0x13bf;
+    pal_obj_mem[11] = 0x1b80;
+    pal_obj_mem[12] = 0x7ea5;
+    pal_obj_mem[13] = 0x4dd0;
+    pal_obj_mem[14] = 0x55df;
+    pal_obj_mem[15] = 0x573f;
+
+    memcpy32(&tile_mem[0][0], gfx_tilesetTiles, gfx_tilesetTilesLen / sizeof(u32));
+    memcpy32(tile_mem_obj[0][0].data, gfx_playerTiles, 32 * 8);
+
+    SCR_ENTRY *se = se_mem[28];
+    for (int y = 0; y < 8; ++y)
+    {
+        for (int x = 0; x < 8; ++x)
+        {
+            int i = ((y << 5) | x) << 1;
+            se[i] = 2;
+            se[++i] = 3;
+
+            i += 32;
+            se[i] = 34;
+            se[++i] = 35;
+        }
+    }
+
+    REG_BG0CNT = BG_CBB(0) | BG_SBB(28) | BG_4BPP | BG_REG_32x32;
+    REG_BG0HOFS = 0;
+    REG_BG0VOFS = 0;
+    REG_DISPCNT = DCNT_OBJ | DCNT_OBJ_1D | DCNT_BG0;
+
+    OBJ_ATTR *obj = &obj_buffer[0];
+    obj_set_attr(obj, ATTR0_SQUARE, ATTR1_SIZE_16, ATTR2_PALBANK(0));
+
+    int x = 96 * 2;
+    int y = 32 * 2;
+    int sx = 0;
+    int sy = 0;
+
+    while (true)
+    {
+        VBlankIntrWait();
+        obj_set_pos(obj, x >> 1, y >> 1);
+        oam_copy(oam_mem, obj_buffer, 1);
+        REG_BG0HOFS = sx;
+        REG_BG0VOFS = sy;
+
+        key_poll();
+
+        if (key_is_down(KEY_RIGHT))
+            sx += 4;
+
+        if (key_is_down(KEY_LEFT))
+            sx -= 4;
+
+        if (key_is_down(KEY_UP))
+            sy -= 4;
+
+        if (key_is_down(KEY_DOWN))
+            sy += 4;
+    }
+
+
+    return 0;
+}
