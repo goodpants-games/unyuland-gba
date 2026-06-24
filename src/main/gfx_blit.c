72f796447438ec284ab6d83a582a58aab01901f6
pkhead <chromosoze@gmail.com>
2026-06-24 10:40:58 -0500

    fix text bitmap fill i think

--- src/main/gfx_blit.c
+++ src/main/gfx_blit.c
@@ -39,16 +39,39 @@ void _gfx_text_blit_tile(uint x, uint y, const TILE4 *src_tile)
     }
 }
 
+// void gfx_text_bmap_fill(uint oc, uint or, uint cols, uint rows, u32 data[8])
+// {
+//     for (uint r = or; r < or + rows; ++r)
+//     {
+//         TILE *t = &GFX_TEXT_BMP_VRAM[r * GFX_TEXT_BMP_COLS + oc];
+//         for (uint c = 0; c < cols; ++c, ++t)
+//         {
+//             for (uint i = 0; i < 8; ++i)
+//                 t->data[i] = data[i];
+//         }
+//     }
+// }
+
+extern bool gfx_text_bmp_dirty_rows[GFX_TEXT_BMP_ROWS];
+
 void gfx_text_bmap_fill(uint oc, uint or, uint cols, uint rows, u32 data[8])
 {
-    for (uint r = or; r < or + rows; ++r)
+    if (cols == 0 || rows == 0) return;
+
+    u32 *tile = (u32 *)(gfx_text_bmp_buf + (GFX_TEXT_BMP_COLS * or + oc));
+    u32 *tile_orig = tile;
+    for (; rows != 0; --rows)
     {
-        TILE *t = &GFX_TEXT_BMP_VRAM[r * GFX_TEXT_BMP_COLS + oc];
-        for (uint c = 0; c < cols; ++c, ++t)
+        gfx_text_bmp_dirty_rows[or] = 1;
+        for (uint ci = cols; ci != 0; --ci)
         {
-            for (uint i = 0; i < 8; ++i)
-                t->data[i] = data[i];
+            memcpy(tile, data, sizeof(u32) * 8);
+            tile += 8;
         }
+
+        tile_orig += GFX_TEXT_BMP_COLS;
+        tile = tile_orig;
+        ++or;
     }
 }
 
